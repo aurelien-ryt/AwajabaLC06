a93c7ed6120ac5c0ba9adfba42be9ed048d13d97
Wisskaii <wisskaii93@gmail.com>
2025-09-03 17:02:53 +0200

    Implémentation du constructeur Repas

--- Repas.java
+++ Repas.java
@@ -2,5 +2,11 @@ public class Repas {
 	public static voix main( String[] args){
 		int numero ;
 		LocalDate date;
+		
+		Repas (int numero, LocalDate date){
+			this.numero = numero;
+			this.date = date;
+			
+			}
 		}	
 	}
