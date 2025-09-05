1eeb1c3df8a6e620c20d46fea38648f8ff099aab
Tatsu <younes.miftah.sio@gmail.com>
2025-09-05 16:51:20 +0200

    Test du depot

--- Repas.java
+++ Repas.java
@@ -1,13 +1,45 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Repas {
-	public static voix main( String[] args){
-		int numero ;
-		LocalDate date;
-		float Paul ;
-		
-		Repas (int numero, LocalDate date){
-			this.numero = numero;
-			this.date = date;
-			
-			}
-		}	
-	}
+    private int numero;      // Le numéro du repas
+    private Date date;       // La date du repas
+
+    // Constructeur
+    public Repas(int numero, Date date) {
+        this.numero = numero;
+        this.date = date;
+    }
+
+    // Getter et Setter pour le numéro
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    // Getter et Setter pour la date
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    // Méthode pour afficher les informations du repas
+    @Override
+    public String toString() {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        return "Repas #" + numero + " - " + sdf.format(date);
+    }
+
+    // Méthode principale pour tester
+    public static void main(String[] args) {
+        // Crée un repas avec numéro 1 et une date spécifique
+        Repas repas1 = new Repas(1, new Date());
+        System.out.println(repas1);
+    }
+}
